deb3e4d181de3c09b55d340b35e22d73bef6edf8
Razvan <razvanmudura25@gmail.com>
2024-08-23 13:39:55 +0300

    commit2

--- .svelte-kit/output/server/manifest-full.js
+++ .svelte-kit/output/server/manifest-full.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["favicon.png","manifest.json"]),
 	mimeTypes: {".png":"image/png",".json":"application/json"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.ZpRNbpfZ.js","app":"_app/immutable/entry/app.3PLUcA-W.js","imports":["_app/immutable/entry/start.ZpRNbpfZ.js","_app/immutable/chunks/scheduler.IlVl7hCi.js","_app/immutable/chunks/singletons.g6zsI8QB.js","_app/immutable/chunks/index.i2bAfgl8.js","_app/immutable/entry/app.3PLUcA-W.js","_app/immutable/chunks/scheduler.IlVl7hCi.js","_app/immutable/chunks/index.U90YCfkg.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		client: {"start":"_app/immutable/entry/start.KKBFCQ8t.js","app":"_app/immutable/entry/app.NRw-1qtK.js","imports":["_app/immutable/entry/start.KKBFCQ8t.js","_app/immutable/chunks/scheduler.IlVl7hCi.js","_app/immutable/chunks/singletons.Vx5ERH9a.js","_app/immutable/chunks/index.i2bAfgl8.js","_app/immutable/entry/app.NRw-1qtK.js","_app/immutable/chunks/scheduler.IlVl7hCi.js","_app/immutable/chunks/index.U90YCfkg.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
