a126939af7ff1c98193b2d4c8506c3aed0d92eb0
Dascalciuc Teodor <dascalciuc.teodor@cylex.ro>
2024-09-25 13:25:03 +0300

    improved advanced buffers styling

--- .svelte-kit/output/server/entries/pages/_page.svelte.js
+++ .svelte-kit/output/server/entries/pages/_page.svelte.js
@@ -1190,7 +1190,6 @@ const AdvancedBuffers = create_ssr_component(($$result, $$props, $$bindings, slo
   let expressionElements = [];
   let inputValue = "";
   let selectedElement = 0;
-  let error = "";
   let { params } = $$props;
   let { schema } = $$props;
   let { value = "" } = $$props;
@@ -1209,6 +1208,7 @@ const AdvancedBuffers = create_ssr_component(($$result, $$props, $$bindings, slo
     value = convertExpressionToValue();
     params.pathChanged(params.path, value || void 0);
   };
+  let buttonRefs = [];
   if ($$props.params === void 0 && $$bindings.params && params !== void 0)
     $$bindings.params(params);
   if ($$props.schema === void 0 && $$bindings.schema && schema !== void 0)
@@ -1285,50 +1285,57 @@ const AdvancedBuffers = create_ssr_component(($$result, $$props, $$bindings, slo
               })}`;
             }
           }
-        )} <div class="flex align-center gap-[10px]"><input class="input" type="search" name="search" placeholder="Search..." autocomplete="off"${add_attribute("value", inputValue, 0)}> <button class="btn variant-filled-primary !text-white" data-svelte-h="svelte-1a4s6bo">Validate</button></div> <span class="${escape("text-rose-600", true) + " text-center font-bold h-[30px]"}">${escape(error)}</span> ${validate_component(TabGroup, "TabGroup").$$render($$result, { class: "max-h-[500px] overflow-auto" }, {}, {
+        )} <div class="flex align-center gap-[10px]"><input class="input" type="search" name="search" placeholder="Search..." autocomplete="off"${add_attribute("value", inputValue, 0)}> <button class="btn variant-filled-primary !text-white" data-svelte-h="svelte-1a4s6bo">Validate</button></div> ${``} <div class="flex gap-2 items-center">${validate_component(Tab, "Tab").$$render(
+          $$result,
+          {
+            class: "w-full",
+            name: "tab1",
+            value: 0,
+            group: tabSet
+          },
+          {
+            group: ($$value) => {
+              tabSet = $$value;
+              $$settled = false;
+            }
+          },
+          {
+            default: () => {
+              return `Buffers`;
+            }
+          }
+        )} ${validate_component(Tab, "Tab").$$render(
+          $$result,
+          {
+            class: "w-full",
+            name: "tab2",
+            value: 1,
+            group: tabSet
+          },
+          {
+            group: ($$value) => {
+              tabSet = $$value;
+              $$settled = false;
+            }
+          },
+          {
+            default: () => {
+              return `Operators`;
+            }
+          }
+        )}</div> ${validate_component(TabGroup, "TabGroup").$$render($$result, { class: "max-h-[200px] overflow-auto" }, {}, {
           panel: () => {
             return `${tabSet === 0 ? `${buffers.map((e) => e.value).filter((e) => e.includes(inputValue)).length > 0 ? `<div class="flex flex-col gap-[10px]">${each(buffers.filter((e) => e.value.includes(inputValue)), (element, index) => {
               return `${index === 0 || index > 0 && buffers[index].category !== buffers[index - 1].category ? `<h1 class="font-bold text-xl">${escape(capitalizeFirstLetter2(element.category))} </h1>` : ``} <button class="${[
                 "text-left btn hover:bg-surface-800 !text-white h-[35px]",
                 index === selectedElement ? "bg-primary-500" : ""
-              ].join(" ").trim()}">${escape(element.value)} </button>`;
-            })}</div>` : `<h1 class="text-center text-xl font-bold" data-svelte-h="svelte-d4g7c6">No buffers found!</h1>`}` : `${tabSet === 1 ? `${operators.map((e) => e.value).filter((e) => e.includes(inputValue)).length > 0 ? `<div class="flex flex-col gap-[10px]">${each(operators, (element, index) => {
+              ].join(" ").trim()}"${add_attribute("this", buttonRefs[index], 0)}>${escape(element.value)} </button>`;
+            })}</div>` : `${buffers.length === 0 ? `<h1 class="text-center text-xl font-bold" data-svelte-h="svelte-2acx19">No chart found!</h1>` : `<h1 class="text-center text-xl font-bold" data-svelte-h="svelte-d4g7c6">No buffers found!</h1>`}`}` : `${tabSet === 1 ? `${operators.map((e) => e.value).filter((e) => e.includes(inputValue)).length > 0 ? `<div class="flex flex-col gap-[10px]">${each(operators, (element, index) => {
               return `${element.value.includes(inputValue) ? `<button class="${[
                 "text-left btn hover:bg-surface-800 !text-white h-[35px]",
                 index === selectedElement ? "bg-primary-500" : ""
-              ].join(" ").trim()}">${escape(element.value)} </button>` : ``}`;
-            })}</div>` : `<h1 class="text-center text-xl font-bold" data-svelte-h="svelte-njnvx8">No operators found!</h1>`}` : ``}`} `;
-          },
-          default: () => {
-            return `${validate_component(Tab, "Tab").$$render(
-              $$result,
-              { name: "tab1", value: 0, group: tabSet },
-              {
-                group: ($$value) => {
-                  tabSet = $$value;
-                  $$settled = false;
-                }
-              },
-              {
-                default: () => {
-                  return `Buffers`;
-                }
-              }
-            )} ${validate_component(Tab, "Tab").$$render(
-              $$result,
-              { name: "tab2", value: 1, group: tabSet },
-              {
-                group: ($$value) => {
-                  tabSet = $$value;
-                  $$settled = false;
-                }
-              },
-              {
-                default: () => {
-                  return `Operators`;
-                }
-              }
-            )}`;
+              ].join(" ").trim()}"${add_attribute("this", buttonRefs[index], 0)}>${escape(element.value)} </button>` : ``}`;
+            })}</div>` : `<h1 class="text-center text-xl font-bold" data-svelte-h="svelte-njnvx8">No operators found!</h1>`}` : ``}`}`;
           }
         })}</div>`}`;
       }
--- .svelte-kit/output/server/manifest.js
+++ .svelte-kit/output/server/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["favicon.png","manifest.json"]),
 	mimeTypes: {".png":"image/png",".json":"application/json"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.iB5lQIVc.js","app":"_app/immutable/entry/app.vOB-Hbmt.js","imports":["_app/immutable/entry/start.iB5lQIVc.js","_app/immutable/chunks/scheduler.IlVl7hCi.js","_app/immutable/chunks/singletons.alkb7e5n.js","_app/immutable/chunks/index.i2bAfgl8.js","_app/immutable/entry/app.vOB-Hbmt.js","_app/immutable/chunks/scheduler.IlVl7hCi.js","_app/immutable/chunks/index.U90YCfkg.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		client: {"start":"_app/immutable/entry/start.aYpXUn9c.js","app":"_app/immutable/entry/app.bDI6mz2K.js","imports":["_app/immutable/entry/start.aYpXUn9c.js","_app/immutable/chunks/scheduler.IlVl7hCi.js","_app/immutable/chunks/singletons.yBb9FEdW.js","_app/immutable/chunks/index.i2bAfgl8.js","_app/immutable/entry/app.bDI6mz2K.js","_app/immutable/chunks/scheduler.IlVl7hCi.js","_app/immutable/chunks/index.U90YCfkg.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
